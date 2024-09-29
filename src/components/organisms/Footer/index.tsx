61e70ced1a050bfb4ffdfec0f6aa59f02eb2bb74
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-09-28 19:35:40 -0500

    refactor: 💄 Update Footer component to display copyright information

---
+++ src/components/organisms/Footer/index.tsx
@@ -0,0 +1,10 @@
+import { TextSecondary } from '@/components/atoms/Titles'
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-white p-3 w-full flex justify-center items-center'>
+      <TextSecondary text='2024 - All rights reserved'/>
+    </div>
+  )
+}
