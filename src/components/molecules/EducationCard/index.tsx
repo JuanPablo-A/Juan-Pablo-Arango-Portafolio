69c6ea20ac699b3d70870ad3a82ccc5d3ef58d1a
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-09-30 07:55:40 -0500

    refactor: 💄 Update EducationCard component layout in PortfolioSection

--- src/components/molecules/EducationCard/index.tsx
+++ src/components/molecules/EducationCard/index.tsx
@@ -11,7 +11,7 @@ type educationProps = {
 
 export default function EducationCard({ university, role, date, title, description }: educationProps) {
   return (
-    <div className='lg:grid lg:grid-cols-2 xl:grid-cols-[1fr_2fr] lg:gap-20 bg-white h-auto md:h-72 lg:h-48 border-b border-solid justify-center items-center w-4/5 lg:w-full'>
+    <div className='lg:grid lg:grid-cols-2 xl:grid-cols-[1fr_2fr] lg:gap-20 bg-white h-auto lg:h-72 xl:h-auto md:h-72 border-b border-solid justify-center items-center w-4/5 lg:w-full pb-2'>
         <div className='flex flex-col justify-center items-center gap-8 mx-10 mt-3 lg:mt-0'>
             <TitleTertiary title={university}/>
             <div className='flex items-center w-full justify-between'>
